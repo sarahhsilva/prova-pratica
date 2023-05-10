a0e49f353826850a0ae13749ee19f0c794f3f760
sarahh <110119890+sarahhsilva@users.noreply.github.com>
2023-05-10 18:26:09 -0300

    Add files via upload

---
+++ prova pratica 3f/app.js
@@ -0,0 +1,62 @@
+const usuarios = [
+    {
+        usuario: 'carlos@carlos.com',
+        senha:'carlos123456'
+    },
+    {
+        usuario: 'leticia@leticia.com',
+        senha:'leticia654321'
+    },
+    {
+        usuario: 'bruno@bruno.com',
+        senha:'bruno987654'
+    },
+]
+const fotografia = [
+    {
+        nome: 'Foto1',
+        like: 0,
+        deslike: 0,
+    },
+    {
+        nome: 'Foto2',
+        like: 0,
+        deslike: 0,
+    },
+    {
+        nome: 'Foto3',
+        like: 0,
+        deslike: 0,
+    },
+    {
+        nome: 'Foto4',
+        like: 0,
+        deslike: 0,
+    },
+    {
+        nome: 'Foto5',
+        like: 0,
+        deslike: 0,
+    },
+    {
+        nome: 'Foto6',
+        like: 0,
+        deslike: 0,
+    },
+    {
+        nome: 'Foto7',
+        like: 0,
+        deslike: 0,
+    },
+    {
+        nome: 'Foto8',
+        like: 0,
+        deslike: 0,
+    },
+    {
+        nome: 'Foto9',
+        like: 0,
+        deslike: 0,
+    },
+    
+]
